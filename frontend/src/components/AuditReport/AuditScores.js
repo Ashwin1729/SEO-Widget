fe90e489496eea669d098026f12a647194d73c2b
Ashwin1729 <mathematicswroteus@gmail.com>
2023-09-18 17:04:52 +0530

    onpage scores animation added

--- frontend/src/components/AuditReport/AuditScores.js
+++ frontend/src/components/AuditReport/AuditScores.js
@@ -1,15 +1,23 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import screenshot from "../../assets/screenshot.png";
 import CircularProgress from "../utils/CircularProgressBar";
 import { Typography } from "@mui/material";
 import styles from "./AuditScores.module.css";
 import { AppContext } from "../../context/application-context";
+import { useCountUp } from "use-count-up";
 
 const AuditScores = () => {
+  // fetching context data
   const appCtx = useContext(AppContext);
   const screenshotUrl = appCtx.screenshotUrl;
   const insightPageData = appCtx.insightPageData;
   const lighthouseData = appCtx.lighthouseData;
+
+  // scores hooks
+  const [onPage, setOnPage] = useState(0);
+  const [performance, setPerformance] = useState(0);
+  const [seo, setSeo] = useState(0);
+  const [bestPractices, setBestPractices] = useState(0);
 
   const onpageScore =
     insightPageData?.tasks[0]?.result[0]?.items[0]?.onpage_score;
@@ -23,6 +31,41 @@ const AuditScores = () => {
 
   console.log(performanceScore, seoScore, bestPracticesScore);
 
+  const { value: onPageValue } = useCountUp({
+    isCounting: true,
+    end: onpageScore,
+    duration: 5,
+    easing: "easeOutCubic",
+  });
+
+  const { value: performancevalue } = useCountUp({
+    isCounting: true,
+    end: performanceScore,
+    duration: 5,
+    easing: "easeOutCubic",
+  });
+
+  const { value: seovalue } = useCountUp({
+    isCounting: true,
+    end: seoScore,
+    duration: 5,
+    easing: "easeOutCubic",
+  });
+
+  const { value: bestPracticesvalue } = useCountUp({
+    isCounting: true,
+    end: bestPracticesScore,
+    duration: 5,
+    easing: "easeOutCubic",
+  });
+
+  useEffect(() => {
+    setOnPage(onPageValue);
+    setPerformance(performancevalue);
+    setSeo(seovalue);
+    setBestPractices(bestPracticesvalue);
+  }, [onPageValue, performancevalue, seovalue, bestPracticesvalue]);
+
   return (
     <div className={styles.score_container}>
       <div className={styles.screenshot}>
@@ -31,7 +74,7 @@ const AuditScores = () => {
       <div className={styles.page_score}>
         <div className={styles.progress}>
           <CircularProgress
-            value={onpageScore}
+            value={onPage}
             style={{ width: "150px", height: "150px" }}
           />
           <Typography variant="h6" gutterBottom>
@@ -41,7 +84,7 @@ const AuditScores = () => {
         <div className={styles.page_score_section}>
           <div className={styles.progress}>
             <CircularProgress
-              value={performanceScore}
+              value={performance}
               style={{ width: "80px", height: "80px" }}
             />
             <Typography variant="h6" gutterBottom>
@@ -50,7 +93,7 @@ const AuditScores = () => {
           </div>
           <div className={styles.progress}>
             <CircularProgress
-              value={seoScore}
+              value={seo}
               style={{ width: "80px", height: "80px" }}
             />
             <Typography variant="h6" gutterBottom>
@@ -59,7 +102,7 @@ const AuditScores = () => {
           </div>
           <div className={styles.progress}>
             <CircularProgress
-              value={bestPracticesScore}
+              value={bestPractices}
               style={{ width: "80px", height: "80px" }}
             />
             <Typography variant="h6" gutterBottom>
